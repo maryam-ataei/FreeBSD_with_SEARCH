fcc4b8933a2f696c6307f7eeb12372cfeb2bde2a
maryam-ataei <ataei92m@gmail.com>
2025-09-17 13:22:39 -0400

    new method for rtt sample with rack

--- cc_newreno_search.h
+++ cc_newreno_search.h
@@ -22,73 +22,74 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- */
+*/
 
- #ifndef _CC_NEWRENO_H
- #define _CC_NEWRENO_H
+#ifndef _CC_NEWRENO_H
+#define _CC_NEWRENO_H
  
 /* SEARCH_begin */
- #define CCALGONAME_NEWRENO "newreno_search"
+#define CCALGONAME_NEWRENO "newreno_search"
  
- #define MAX_US_INT 0xffff
+#define MAX_US_INT 0xffff
  
- typedef uint32_t search_bin_t;
+typedef uint32_t search_bin_t;
  
- #define V_use_search 1
- #define V_use_hystartpp 0
+#define V_use_search 1
+#define V_use_hystartpp 0
 
- #define V_CWND_ROLLBACK 0
+#define V_CWND_ROLLBACK 0
  
- #define SEARCH_WINDOW_SIZE_FACTOR 35
- #define SEARCH_BINS 10
- #define SEARCH_EXTRA_BINS 15
- #define SEARCH_TOTAL_BINS (SEARCH_BINS + SEARCH_EXTRA_BINS)
- #define SEARCH_THRESH 35
- #define SEARCH_ALPHA MAX_US_INT
- 
- enum unset_bin_duration {
-	 RESET_BIN_DURATION_TRUE,		// Reset bin duration
-	 RESET_BIN_DURATION_FALSE		// Do not reset bin duration
- };
+#define SEARCH_WINDOW_SIZE_FACTOR 35
+#define SEARCH_BINS 10
+#define SEARCH_EXTRA_BINS 15
+#define SEARCH_TOTAL_BINS (SEARCH_BINS + SEARCH_EXTRA_BINS)
+#define SEARCH_THRESH 35
+#define SEARCH_ALPHA MAX_US_INT
+
+enum unset_bin_duration {
+	RESET_BIN_DURATION_TRUE,		// Reset bin duration
+	RESET_BIN_DURATION_FALSE		// Do not reset bin duration
+};
  
 /* SEARCH_end */
  
- struct newreno {
-	 uint32_t beta;
-	 uint32_t beta_ecn;
-	 uint32_t newreno_flags;
-	 uint32_t css_baseline_minrtt;
-	 uint32_t css_current_round_minrtt;
-	 uint32_t css_lastround_minrtt;
-	 uint32_t css_rttsample_count;
-	 uint32_t css_entered_at_round;
-	 uint32_t css_current_round;
-	 uint32_t css_fas_at_css_entry;
-	 uint32_t css_lowrtt_fas;
-	 uint32_t css_last_fas;
+struct newreno {
+	uint32_t beta;
+	uint32_t beta_ecn;
+	uint32_t newreno_flags;
+	uint32_t css_baseline_minrtt;
+	uint32_t css_current_round_minrtt;
+	uint32_t css_lastround_minrtt;
+	uint32_t css_rttsample_count;
+	uint32_t css_entered_at_round;
+	uint32_t css_current_round;
+	uint32_t css_fas_at_css_entry;
+	uint32_t css_lowrtt_fas;
+	uint32_t css_last_fas;
  
-	 /* SEARCH_begin */
-	 uint32_t search_bin_duration_us;				/* duration of each bin in microsecond */
-	 int32_t  search_curr_idx;						/* total number of bins */
-	 uint64_t search_bin_end_us;					/* end time of the latest bin in microsecond */
-	 search_bin_t search_bin[SEARCH_TOTAL_BINS];	/* array to keep bytes for bins */
-	 uint8_t search_scale_factor;					/* scale factor to fit the value with bin size */
-	 uint32_t search_bytes_curr_bin;				/* bytes_acked during this bin*/
- };
+	/* SEARCH_begin */
+	uint32_t last_rtt_sample;					/* Most recent RTT sample (in microseconds) from rttsample() */
+	uint32_t search_bin_duration_us;			/* duration of each bin in microsecond */
+	int32_t  search_curr_idx;					/* total number of bins */
+	uint64_t search_bin_end_us;					/* end time of the latest bin in microsecond */
+	search_bin_t search_bin[SEARCH_TOTAL_BINS];	/* array to keep bytes for bins */
+	uint8_t search_scale_factor;				/* scale factor to fit the value with bin size */
+	uint32_t search_bytes_curr_bin;				/* bytes_acked during this bin*/
+};
  
- #define SEARCH_BIN(ccv, index) ((struct newreno*)(ccv)->cc_data)->search_bin[(index) % SEARCH_TOTAL_BINS]
- /* SEARCH_end */
+#define SEARCH_BIN(ccv, index) ((struct newreno*)(ccv)->cc_data)->search_bin[(index) % SEARCH_TOTAL_BINS]
+/* SEARCH_end */
 
- struct cc_newreno_opts {
-	 int		name;
-	 uint32_t	val;
- };
+struct cc_newreno_opts {
+	int		name;
+	uint32_t	val;
+};
  
- #define CC_NEWRENO_BETA			1	/* Beta for normal DUP-ACK/Sack recovery */
- #define CC_NEWRENO_BETA_ECN		2	/* ECN Beta for Abe */
+#define CC_NEWRENO_BETA			1	/* Beta for normal DUP-ACK/Sack recovery */
+#define CC_NEWRENO_BETA_ECN		2	/* ECN Beta for Abe */
  
- /* Flags values */
- #define CC_NEWRENO_HYSTART_ENABLED		0x0002	/* We can do hystart, a loss removes this flag */
- #define CC_NEWRENO_HYSTART_IN_CSS		0x0004	/* If we enter hystart CSS this flag is set */
- #define CC_NEWRENO_BETA_ECN_ENABLED 	0x0020
- #endif /* _CC_NEWRENO_H */
+/* Flags values */
+#define CC_NEWRENO_HYSTART_ENABLED		0x0002	/* We can do hystart, a loss removes this flag */
+#define CC_NEWRENO_HYSTART_IN_CSS		0x0004	/* If we enter hystart CSS this flag is set */
+#define CC_NEWRENO_BETA_ECN_ENABLED 	0x0020
+#endif /* _CC_NEWRENO_H */
