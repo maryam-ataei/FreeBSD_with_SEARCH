788c6dd643873286690e1184fadacb6aa7b442ec
maryam-ataei <ataei92m@gmail.com>
2025-09-25 15:58:10 -0400

    update logging

--- cc_newreno_search.c
+++ cc_newreno_search.c
@@ -666,22 +666,24 @@ search_update(struct cc_var* ccv) {
 			}
 		}
 
-		#ifdef SEARCH_LOG_ENABLED
-			log(LOG_INFO, 
-				"[CCRG]: [flow_pointer: %p] SEARCH_INFO: [now %lu] [bin_duration %d] "
-				"[bin_end %lu] [curr_delv %ld] [prev_delv %ld] [norm_100 %d] "
-				"[scale_factor %d] [curr_idx %d]\n",
-				ccv, 
-				now_us, 
-				nreno->search_bin_duration_us, 
-				nreno->search_bin_end_us, 
-				curr_delv_bytes,
-				prev_delv_bytes,
-				norm_diff,
-				nreno->search_scale_factor,
-				nreno->search_curr_idx);
-		#endif
 	}
+
+	#ifdef SEARCH_LOG_ENABLED
+	log(LOG_INFO, 
+		"[CCRG]: [flow_pointer: %p] SEARCH_INFO: [now %lu] [bin_duration %d] "
+		"[bin_end %lu] [curr_delv %ld] [prev_delv %ld] [norm_100 %d] "
+		"[scale_factor %d] [curr_idx %d]\n",
+		ccv, 
+		now_us, 
+		nreno->search_bin_duration_us, 
+		nreno->search_bin_end_us, 
+		curr_delv_bytes,
+		prev_delv_bytes,
+		norm_diff,
+		nreno->search_scale_factor,
+		nreno->search_curr_idx);
+	#endif
+
 	return false;
 }
 /* SEARCH_end */
